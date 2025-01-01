10ccb81d5bcb5e11921e6db3a61f12a26c380b65
Elekrisk <einar.vilhelm.persson@gmail.com>
2025-01-01 13:44:34 +0100

    Revert "Add LOS requirement for stalker attack"
    The check doesn't work :(
    
    This reverts commit 3dd07d978448f6dba210c36ef25377c8240212fb.

--- Plugin/src/Stalker/StalkerAI.cs
+++ Plugin/src/Stalker/StalkerAI.cs
@@ -135,7 +135,7 @@ namespace IPMaster.Stalker
                         StartSearch(transform.position);
                         TargetPlayer(null);
                         SwitchToBehaviourState((int)State.Searching);
-                    } else if (Vector3.Distance(targetPlayer.transform.position, transform.position) < 2f && timeSinceLastAttack >= 0.25f && CheckLineOfSightForPosition(targetPlayer.transform.position)) {
+                    } else if (Vector3.Distance(targetPlayer.transform.position, transform.position) < 2f && timeSinceLastAttack >= 0.25f) {
                         timeSinceLastAttack = 0.0f;
                         DamagePlayerClientRpc(targetPlayer.playerClientId, 20);
                     }
